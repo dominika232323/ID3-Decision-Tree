f1f99e9ca6a9d322664d07635bb3980a1ec6f854
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-10 00:27:07 +0100

    Reimplement Node class

--- node.py
+++ node.py
@@ -1,10 +1,4 @@
 class Node:
-    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+    def __init__(self, feature=None, children=None):
         self.feature = feature
-        self.threshold = threshold
-        self.left = left
-        self.right = right
-        self.value = value
-
-    def is_leaf_node(self):
-        return self.value is not None
+        self.children = children if children is not None else []
