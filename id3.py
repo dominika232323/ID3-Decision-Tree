4426c4cb83c1bf440bbf0534a8d1a7a6634b4455
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-09 18:11:17 +0100

    Implement count_subset_entropy and test inf_gain

--- id3.py
+++ id3.py
@@ -20,11 +20,49 @@ def count_entropy(U):
     for name in classnames:
         class_count = classes.value_counts().get(name)
         ratio = class_count / num_of_rows
-        class_entropy = -1 * ratio * np.log2(ratio)
+
+        if ratio == 0 or ratio == 1:
+            class_entropy = 0
+        else:
+            class_entropy = -1 * ratio * np.log2(ratio)
+
         I += class_entropy
 
     return I
 
 
 def count_subset_entropy(d, U):
-    pass
+    inf = 0
+
+    column = U[U.columns[d]]
+    column_values = column.unique()
+
+    classes = U[U.columns[-1]]
+    classnames = classes.unique()
+
+    num_of_rows = len(U)
+
+    for value in column_values:
+        value_count = column.value_counts().get(value)
+
+        class_count = {name: 0 for name in classnames}
+
+        for val, cla in zip(column, classes):
+            if val == value:
+                class_count[cla] += 1
+
+        subset_entropy = 0
+
+        for name in classnames:
+            ratio = class_count[name] / value_count
+
+            if ratio == 0 or ratio == 1:
+                ent = 0
+            else:
+                ent = -1 * ratio * np.log2(ratio)
+
+            subset_entropy += ent
+
+        inf += value_count / num_of_rows * subset_entropy
+
+    return inf
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from id3 import *
 if __name__ == '__main__':
     data = pd.read_csv('data/test.data', sep=',', dtype=str, header=None)
 
-    print(count_entropy(data))
+    print(inf_gain(0, data))
