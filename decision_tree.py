3b6930ab7f994ec62555c2994d6c5c5f9c024001
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-10 01:20:43 +0100

    Initiate decision tree

--- decision_tree.py
+++ decision_tree.py
@@ -1,3 +1,12 @@
+from id3 import id3
+
+
 class DecisionTree:
-    def __int__(self, root):
+    def __int__(self, root=None):
         self.root = root
+
+    def build_id3_tree(self, data):
+        pass
+
+    def predict(self, data):
+        pass
