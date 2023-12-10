0fbf15719790a6863ec4a80d687bd22724098c9e
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-10 19:34:05 +0100

    Add build_id3_tree method

--- decision_tree.py
+++ decision_tree.py
@@ -2,11 +2,18 @@ from id3 import id3
 
 
 class DecisionTree:
-    def __int__(self, root=None):
-        self.root = root
+    def __init__(self):
+        self.root = None
 
-    def build_id3_tree(self, data):
-        pass
+    def build_id3_tree(self, dataset, class_column_index):
+        class_column = dataset[dataset.columns[class_column_index]]
+        class_names = class_column.unique()
+
+        num_of_columns = dataset.shape[1]
+        informative_features = [i for i in range(num_of_columns)]
+        informative_features.remove(class_column_index)
+
+        self.root = id3(class_names, informative_features, dataset)
 
     def predict(self, data):
         pass
