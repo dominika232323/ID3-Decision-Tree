7e70ccba63606831cbb95424e6363edd851b9106
dominika232323 <dominikaboguszewska62@gmail.com>
2023-12-10 20:19:24 +0100

    Add add_child method

--- node.py
+++ node.py
@@ -1,4 +1,7 @@
 class Node:
-    def __init__(self, feature=None, children=None):
+    def __init__(self, feature, children=None):
         self.feature = feature
         self.children = children if children is not None else []
+
+    def add_child(self, child):
+        self.children.append(child)
